29b9bba72aceb67358da21fe4557b57136189f54
Matt Boisseau <boissemr@miamioh.edu>
2015-12-05 01:16:18 -0500

    "fixed" the bug in which sometimes two copies were created
    hell if I know why it was happening, so now there is a stupid timer hack
    to make it look fixed

--- Assets/scripts/TotemController.cs
+++ Assets/scripts/TotemController.cs
@@ -12,25 +12,43 @@ public class TotemController : MonoBehaviour {
 	Transform[]		oldChildren,
 					newChildren;
 	GameObject[]	groups;
+	float			DEBUG_TIMER;
 
 	// start
 	void Start() {
 		isUsed = false;
+		DEBUG_TIMER = 10;
+	}
+
+	// activate
+	void Awake() {
+		DEBUG_TIMER = 10;
 	}
 
 	// update
 	void Update() {
+
 		// make this totem invisible
 		if(isUsed) {
 			foreach(MeshRenderer o in this.transform.parent.gameObject.GetComponentsInChildren<MeshRenderer>()) o.enabled = false;
 			foreach(Light o in this.transform.parent.gameObject.GetComponentsInChildren<Light>()) o.enabled = false;
 		}
+
+		// DEBUG TIMER
+		DEBUG_TIMER -= 1;
 	}
 
 	// trigger enter
 	void OnTriggerEnter(Collider c) {
+
+		// DEBUG TIMER
+		if(c.gameObject.tag == "Player" && DEBUG_TIMER > 0) {
+			isUsed = true;
+		}
+
 		//if the player triggers the totem
-		if (c.gameObject.tag == "Player" && !isUsed) {
+		if(c.gameObject.tag == "Player" && !isUsed && DEBUG_TIMER <= 0) {
+
 			// activate first unused group
 			groups = GameObject.Find("sceneController").GetComponent<SceneController>().GetGroups();
 			for(int i = 0; i < groups.Length; i++) {
@@ -53,43 +71,68 @@ public class TotemController : MonoBehaviour {
 
 					// match the attributes of children from old to new
 					for(int j = 0; j < oldChildren.Length; j++) {
+
+						/*
+						 * !NOTE
+						 * to fix issue with enemy jumping
+						 * all enemies must have agents disabled when player's state is copied
+						 * maybe disable enemy agents in Awake()?
+						 */
+
+						// get this child's old and new instances
+						GameObject o = oldChildren[j].gameObject;
+						GameObject n = newChildren[j].gameObject;
 						
-						// enemies
-						if(oldChildren[j].gameObject.layer == 10) {
-							Transform				n = newChildren[j],
-													o = oldChildren[j];
-							EnemyAgentController	nController = n.gameObject.GetComponent<EnemyAgentController>(),
-													oController = o.gameObject.GetComponent<EnemyAgentController>();
-							n.localPosition = o.localPosition;
-							nController.health = oController.health;
-							nController.GetComponent<NavMeshAgent>().enabled = true;
+						// totems
+						/*
+						 * !NOTE
+						 * something seriously janky happens here if we are copying from the original sreen
+						 * why does it happen?
+						 */
+						/*
+						if(o.name == "totemCollider") {
+
+							// get controllers
+							TotemController			nController = n.GetComponent<TotemController>(),
+													oController = o.GetComponent<TotemController>();
+
+							// copy state from old to new
+							nController.isUsed = oController.isUsed;
 						}
+						*/
 						
 						// target
-						if(oldChildren[j].gameObject.layer == 9) {
-							Transform				n = newChildren[j],
-													o = oldChildren[j];
-							n.localPosition = o.localPosition;
+						if(o.layer == 9) {
+							
+							// copy state from old to new
+							n.transform.localPosition = o.transform.localPosition;
 						}
 						
 						// player						
-						if(oldChildren[j].gameObject.layer == 8) {
-							Transform				n = newChildren[j],
-													o = oldChildren[j];
-							AgentController			nController = n.gameObject.GetComponent<AgentController>(),
-													oController = o.gameObject.GetComponent<AgentController>();
-							n.localPosition = o.localPosition;
+						if(o.layer == 8) {
+							
+							// get controllers
+							AgentController			nController = n.GetComponent<AgentController>(),
+													oController = o.GetComponent<AgentController>();
+							
+							// copy state from old to new
+							n.transform.localPosition = o.transform.localPosition;
 							nController.health = oController.health;
 							nController.GetComponent<NavMeshAgent>().enabled = true;
+							nController.target.transform.localPosition = oController.target.transform.localPosition;
 						}
 						
-						// totems
-						if(oldChildren[j].gameObject.name == "totemCollider") {
-							Transform				n = newChildren[j],
-													o = oldChildren[j];
-							TotemController			nController = n.gameObject.GetComponent<TotemController>(),
-													oController = o.gameObject.GetComponent<TotemController>();
-							nController.isUsed = oController.isUsed;
+						// enemies
+						if(o.layer == 10) {
+							
+							// get controllers
+							EnemyAgentController	nController = n.GetComponent<EnemyAgentController>(),
+													oController = o.GetComponent<EnemyAgentController>();
+							
+							// copy state from old to new
+							n.transform.localPosition = o.transform.localPosition;
+							nController.health = oController.health;
+							nController.GetComponent<NavMeshAgent>().enabled = true;
 						}
 					}
 

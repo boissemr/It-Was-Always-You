86a239e3b593ac87ebe73c0355e85b7faadacd1c
Matt Boisseau <boissemr@miamioh.edu>
2015-12-06 14:48:28 -0500

    added health indicators
    each screen now displays the health of its respective player object

---
+++ Assets/scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public GameObject player;
+
+	Text t;
+
+	void Start() {
+		t = GetComponentInChildren<Text>();
+	}
+
+	void Update() {
+		if(player.activeSelf) {
+			t.text = player.GetComponent<AgentController>().health.ToString();
+		} else {
+			t.text = "0";
+		}
+	}
+}
